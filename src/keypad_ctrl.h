be27b80d28ccdfa262cd36cddb22562532195876
Ellen Sun <sunellenyuan@gmail.com>
2025-04-24 11:36:11 -0400

    Add function docs

--- src/keypad_ctrl.h
+++ src/keypad_ctrl.h
@@ -9,8 +9,20 @@ extern "C" {
     #include <stdint.h>
 #endif
 
+
+/**
+ * Function to initialize and setup keypad.
+ */
 void keypad_setup();
 
+
+/**
+ * Function to flash an LED on and off.
+ * 
+ * @param led: The integer representing an LED button on the keypad to flash
+ * @param time_on: An integer in ms representing the amount of time the LED 
+ * will spend on and off
+ */
 void flash_led(int led, int time_on);
 
 #ifdef __cplusplus
