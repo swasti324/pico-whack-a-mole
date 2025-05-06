512800007c4ef488d296bd39929f69cfe1890b1f
Ellen Sun <sunellenyuan@gmail.com>
2025-05-06 00:35:23 -0400

    fix clang tidy

--- src/keypad_ctrl.cpp
+++ src/keypad_ctrl.cpp
@@ -1,3 +1,5 @@
+//NOLINTBEGIN(llvmlibc-implementation-in-namespace)
+
 #include "keypad_ctrl.h"
 #include "pico/stdlib.h"
 #include "pico_rgb_keypad.hpp"
@@ -7,21 +9,23 @@
 #include <cstdio>
 #include <cstdlib>
 
-
 using namespace pimoroni;
 
+//NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 PicoRGBKeypad keypad;
 
 struct color {uint8_t r, g, b;};
 
-color RED  = {100, 0, 0};
-color GREEN  = {0, 100, 0};
-color PURPLE  = {100, 0, 100};
-color OFF  = {50, 50, 50};
+const color RED  = {100, 0, 0};
+const color GREEN  = {0, 100, 0};
+const color PURPLE  = {100, 0, 100};
+const color OFF  = {50, 50, 50};
+
+const float BRIGHTNESS = 0.1F;
 
 void keypad_setup(void) {
     keypad.init(); // Set up GPIO
-    keypad.set_brightness(0.1f);
+    keypad.set_brightness(BRIGHTNESS);
 
     // Initialize all LEDs to a dim whitish while "off"
     for(int i = 0; i < keypad.NUM_PADS; i++) {
@@ -42,11 +46,13 @@ void turnoff_led(int led, int* is_led_on) {
     *is_led_on = 0;
 }
 
+//NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
 void flash_status(int led, int correct) {
-    if (correct) {
+    const int flash_time = 400;
+    if (correct == 1) {
         keypad.illuminate(led, GREEN.r, GREEN.g, GREEN.b); // green
         keypad.update();
-        sleep_ms(400);
+        sleep_ms(flash_time);
     }
     else {
         for(int i = 0; i < keypad.NUM_PADS; i++) {
@@ -61,16 +67,17 @@ uint16_t get_buttons(void) {
 }
 
 int button_pressed(uint16_t cur_states, uint16_t prev_states) {
-    uint16_t changed_buttons = cur_states ^ prev_states;
-    uint16_t pressed_buttons = cur_states & changed_buttons;
+    const uint16_t changed_buttons = cur_states ^ prev_states;
+    const uint16_t pressed_buttons = cur_states & changed_buttons;
 
     for (int i = 0; i < keypad.NUM_PADS; i++) {
-        if (pressed_buttons & (1 << i)) { 
-            int pressed_button = i; 
-            return pressed_button;
+        //NOLINTNEXTLINE(hicpp-signed-bitwise)
+        if ((pressed_buttons & (1 << i)) != 0) { 
+            return i;
         }
     }
 
     return -1;
 }
 
+//NOLINTEND(llvmlibc-implementation-in-namespace)
