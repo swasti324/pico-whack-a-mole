c86b281e37edd8fbbeaff9c6861152aa4f685a0e
Ellen Sun <sunellenyuan@gmail.com>
2025-05-06 00:42:31 -0400

    clang format

--- src/keypad_ctrl.cpp
+++ src/keypad_ctrl.cpp
@@ -1,83 +1,83 @@
-//NOLINTBEGIN(llvmlibc-implementation-in-namespace)
+// NOLINTBEGIN(llvmlibc-implementation-in-namespace)
 
 #include "keypad_ctrl.h"
-#include "pico/stdlib.h"
-#include "pico_rgb_keypad.hpp"
 
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 
+#include "pico/stdlib.h"
+#include "pico_rgb_keypad.hpp"
+
 using namespace pimoroni;
 
-//NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 PicoRGBKeypad keypad;
 
-struct color {uint8_t r, g, b;};
+struct color {
+  uint8_t r, g, b;
+};
 
-const color RED  = {100, 0, 0};
-const color GREEN  = {0, 100, 0};
-const color PURPLE  = {100, 0, 100};
-const color OFF  = {50, 50, 50};
+const color RED = {100, 0, 0};
+const color GREEN = {0, 100, 0};
+const color PURPLE = {100, 0, 100};
+const color OFF = {50, 50, 50};
 
 const float BRIGHTNESS = 0.1F;
 
 void keypad_setup(void) {
-    keypad.init(); // Set up GPIO
-    keypad.set_brightness(BRIGHTNESS);
+  keypad.init();  // Set up GPIO
+  keypad.set_brightness(BRIGHTNESS);
 
-    // Initialize all LEDs to a dim whitish while "off"
-    for(int i = 0; i < keypad.NUM_PADS; i++) {
-        keypad.illuminate(i, OFF.r, OFF.g, OFF.b);
-    }
-    keypad.update();
+  // Initialize all LEDs to a dim whitish while "off"
+  for (int i = 0; i < keypad.NUM_PADS; i++) {
+    keypad.illuminate(i, OFF.r, OFF.g, OFF.b);
+  }
+  keypad.update();
 }
 
-void turnon_led (int led, int* is_led_on) {
-    keypad.illuminate(led, PURPLE.r, PURPLE.g, PURPLE.b);
-    keypad.update();
-    *is_led_on = 1;
+void turnon_led(int led, int* is_led_on) {
+  keypad.illuminate(led, PURPLE.r, PURPLE.g, PURPLE.b);
+  keypad.update();
+  *is_led_on = 1;
 }
 
 void turnoff_led(int led, int* is_led_on) {
-    keypad.illuminate(led, OFF.r, OFF.g, OFF.b);
-    keypad.update();
-    *is_led_on = 0;
+  keypad.illuminate(led, OFF.r, OFF.g, OFF.b);
+  keypad.update();
+  *is_led_on = 0;
 }
 
-//NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
 void flash_status(int led, int correct) {
-    const int flash_time = 400;
-    if (correct == 1) {
-        keypad.illuminate(led, GREEN.r, GREEN.g, GREEN.b); // green
-        keypad.update();
-        sleep_ms(flash_time);
+  const int flash_time = 400;
+  if (correct == 1) {
+    keypad.illuminate(led, GREEN.r, GREEN.g, GREEN.b);  // green
+    keypad.update();
+    sleep_ms(flash_time);
+  } else {
+    for (int i = 0; i < keypad.NUM_PADS; i++) {
+      keypad.illuminate(i, RED.r, RED.g, RED.b);  // everything red
+      keypad.update();
     }
-    else {
-        for(int i = 0; i < keypad.NUM_PADS; i++) {
-            keypad.illuminate(i, RED.r, RED.g, RED.b); // everything red
-            keypad.update();
-        }
-    }
+  }
 }
 
-uint16_t get_buttons(void) {
-    return keypad.get_button_states();
-}
+uint16_t get_buttons(void) { return keypad.get_button_states(); }
 
 int button_pressed(uint16_t cur_states, uint16_t prev_states) {
-    const uint16_t changed_buttons = cur_states ^ prev_states;
-    const uint16_t pressed_buttons = cur_states & changed_buttons;
+  const uint16_t changed_buttons = cur_states ^ prev_states;
+  const uint16_t pressed_buttons = cur_states & changed_buttons;
 
-    for (int i = 0; i < keypad.NUM_PADS; i++) {
-        //NOLINTNEXTLINE(hicpp-signed-bitwise)
-        if ((pressed_buttons & (1 << i)) != 0) { 
-            return i;
-        }
+  for (int i = 0; i < keypad.NUM_PADS; i++) {
+    // NOLINTNEXTLINE(hicpp-signed-bitwise)
+    if ((pressed_buttons & (1 << i)) != 0) {
+      return i;
     }
+  }
 
-    return -1;
+  return -1;
 }
 
-//NOLINTEND(llvmlibc-implementation-in-namespace)
+// NOLINTEND(llvmlibc-implementation-in-namespace)
--- src/keypad_ctrl.h
+++ src/keypad_ctrl.h
@@ -4,61 +4,64 @@
 // make it so that C can use these functions
 #ifdef __cplusplus
 extern "C" {
-    #include <cstdint>
+#include <cstdint>
 #else
-    #include <stdint.h>
+#include <stdint.h>
 #endif
-
 
 /**
  * Function to initialize and setup keypad.
  */
 void keypad_setup(void);
 
-
 /**
  * Function to turn on a LED.
- * 
+ *
  * @param led: The integer representing an LED button on the keypad to turn on
  * @param is_led_on: A pointer to an integer (0 or 1) representing the current
- * on state of the LED. 
+ * on state of the LED.
  */
-void turnon_led (int led, int* is_led_on);
+void turnon_led(int led, int* is_led_on);
 
 /**
  * Function to turn off a LED.
- * 
+ *
  * @param led: The integer representing an LED button on the keypad to turn off
  * @param is_led_on: A pointer to an integer (0 or 1) representing the current
- * on state of the LED. 
+ * on state of the LED.
  */
-void turnoff_led (int led, int* is_led_on);
+void turnoff_led(int led, int* is_led_on);
 
 /**
- * Function to flash the status of the LED pressed (correct or incorrect). The LED will
- * flash green if it's the correct one, and all will flash red if it's incorrect.
- * 
+ * Function to flash the status of the LED pressed (correct or incorrect). The
+ * LED will flash green if it's the correct one, and all will flash red if it's
+ * incorrect.
+ *
  * @param led: The integer representing an LED button on the keypad to flash
- * @param correct: An integer representing if the LED pressed was correct (1) or not (0)
+ * @param correct: An integer representing if the LED pressed was correct (1) or
+ * not (0)
  */
 void flash_status(int led, int correct);
 
 /**
- * Function to find which button 0-15 has been pressed. Only works for one press,
- * and returns only the first one from 0 to 15 if mulitple are pressed at the same time.
- * Returns -1 if none are pressed.
- * 
- * @param cur_states An unsigned 16-bit integer containing current button states, each bit
- * representing if the button is currently being pressed (1) or not (0)
- * @param prev_states An unsigned 16-bit integer containing previous button states
- * 
- * @return An integer (0-15) representing which button was pressed, or -1 if none are pressed
+ * Function to find which button 0-15 has been pressed. Only works for one
+ * press, and returns only the first one from 0 to 15 if mulitple are pressed at
+ * the same time. Returns -1 if none are pressed.
+ *
+ * @param cur_states An unsigned 16-bit integer containing current button
+ * states, each bit representing if the button is currently being pressed (1) or
+ * not (0)
+ * @param prev_states An unsigned 16-bit integer containing previous button
+ * states
+ *
+ * @return An integer (0-15) representing which button was pressed, or -1 if
+ * none are pressed
  */
 int button_pressed(uint16_t cur_states, uint16_t prev_states);
 
 /**
  * Function to get current button states.
- * 
+ *
  * @return An unsigned 16-bit integer containing current button states, each bit
  * representing if the button is currently being pressed (1) or not (0)
  */
